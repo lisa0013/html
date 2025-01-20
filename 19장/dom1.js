b76045e21f9cf6f6ccd544d10b1df4524ba94106
youngseo <vnslduath@naver.com>
2025-01-20 22:50:55 +0900

    자바스크립트 다섯째날 복습 완료

--- 19장/dom1.js
+++ 19장/dom1.js
@@ -13,7 +13,10 @@ document.addEventListener('DOMContentLoaded', function() {
     // document.querySelector('#detail>ul').innerHTML += '<li>가공법 : 워시드</li>';
     let item =  document.createElement('li'); // Element 생성.
     let txt = document.createTextNode('가공법 : 워시드'); // TextNode 생성.
+    
     item.appendChild(txt); // 부모.appendChild(자식)
+
+    
     console.log(item);
     document.querySelector('#detail>ul').appendChild(item);
   });
